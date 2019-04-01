649c2a24694daacf2c625c3fa49f5cd708535790
stanleyondrus <stanley.ondrus@gmail.com>
2019-03-31 23:06:13 -0400

    pthread creation

--- assignment4-5.c
+++ assignment4-5.c
@@ -23,7 +23,7 @@
 #ifdef BGQ
 #include<hwi/include/bqc/A2_inlines.h>
 #else
-#define GetTimeBase MPI_Wtime            
+#define GetTimeBase MPI_Wtime
 #endif
 
 /***************************************************************************/
@@ -40,8 +40,8 @@ typedef unsigned short int bool;
 
 double g_time_in_secs = 0;
 double g_processor_frequency = 1600000000.0; // processing speed for BG/Q
-unsigned long long g_start_cycles=0;
-unsigned long long g_end_cycles=0;
+unsigned long long g_start_cycles = 0;
+unsigned long long g_end_cycles = 0;
 
 int uni_rows = 1024;
 int uni_cols = 1024;
@@ -53,39 +53,47 @@ int gol_ticks = 10;
 /***************************************************************************/
 
 bool **alloc_bool_arr_2d(int rows, int cols);
+
 void init_universe(bool **uni, int rows, int cols);
+
 void sync_ghost_rows(bool **old_uni, int rows, int cols, int rank, int num_ranks);
+
 void update_universe_state(bool **old_uni, int rows, int cols, int rank, int num_ranks, double thresh);
+
 bool life_lotto(Gen seed);
+
 bool cell_next_state(int i, int j, bool **uni, int rows, int cols);
+
 int neighbor_count(int i_in, int j_in, bool **uni, int rows, int cols);
+
 void print_some_universe(bool **uni);
+
 void free_bool_arr_2d(bool **uni, int rows);
 
+void *threadFunction(void *arg);
 
 /***************************************************************************/
 /* Function: Main **********************************************************/
 /***************************************************************************/
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     int mpi_myrank;
     int mpi_ranks;
     // Example MPI startup and using CLCG4 RNG
-    MPI_Init( &argc, &argv);
-    MPI_Comm_size( MPI_COMM_WORLD, &mpi_ranks);
-    MPI_Comm_rank( MPI_COMM_WORLD, &mpi_myrank);
-    
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &mpi_ranks);
+    MPI_Comm_rank(MPI_COMM_WORLD, &mpi_myrank);
+
     // Init 32,768 RNG streams - each rank has an independent stream
     InitDefault();
-    
+
     // Note, used the mpi_myrank to select which RNG stream to use.
     // You must replace mpi_myrank with the right row being used.
     // This just show you how to call the RNG.    
-    printf("Rank %d of %d has been started and a first Random Value of %lf\n", 
-	   mpi_myrank, mpi_ranks, GenVal(mpi_myrank));
-    
-    MPI_Barrier( MPI_COMM_WORLD );
+    printf("Rank %d of %d has been started and a first Random Value of %lf\n",
+           mpi_myrank, mpi_ranks, GenVal(mpi_myrank));
+
+    MPI_Barrier(MPI_COMM_WORLD);
 
     // Start here
     int rows_per_rank = uni_rows / mpi_ranks;
@@ -96,25 +104,61 @@ int main(int argc, char *argv[])
 
     // TODO: Launch pthreads
 
+    int thread_count = 63;
+    pthread_t tid[thread_count];
+    int tid_index = 0;
+    int rc;
+
+    bool *arg = malloc(sizeof(bool));
+    *arg = 69;
+
+    for (int i = 0; i < thread_count; i++) {
+        rc = pthread_create(&tid[tid_index], NULL, threadFunction, arg);
+        if (rc != 0) { printf("Could not create thread"); }
+        tid_index++;
+    }
+
     // Run GoL simulation
     for (int t = 0; t < 1000; t++) {
         sync_ghost_rows(universe, rows_per_rank + 2, uni_cols, mpi_myrank, mpi_ranks);
-        if (t == 0)
+        if (t == 0) {
             update_universe_state(universe, rows_per_rank + 2, uni_cols, mpi_myrank, mpi_ranks, 0.70f);
-        else
+        }
+        else {
             update_universe_state(universe, rows_per_rank + 2, uni_cols, mpi_myrank, mpi_ranks, 0.0f);
-        if (mpi_myrank == 0 && t%100 == 0) {
+        }
+        if (mpi_myrank == 0 && t % 100 == 0) {
             printf("Tick: %d\n", t);
             print_some_universe(universe);
         }
         // TODO: Write current state to file
+
+
+//        int a = 4, b;
+//        MPI_File fh;
+//        MPI_File_open( MPI_COMM_WORLD, "myfile", MPI_MODE_RDWR, MPI_INFO_NULL, &fh ) ;
+//        MPI_File_set_view( fh, 0, MPI_INT, MPI_INT, "native", MPI_INFO_NULL ) ;
+//        MPI_File_write_at(fh, 10, &a, 1, MPI_INT, &status ) ;
+//        MPI_File_read_at(fh,  10, &b, 1, MPI_INT, &status ) ;
+//        MPI_File_close( &cFile );
+
     }
+
+
+    bool *ret_val;
+    for (int i = 0; i < tid_index; i++) {
+        rc = pthread_join(tid[i], (void **) &ret_val);
+        if (rc != 0) { printf("Could not join thread"); }
+        printf("THREAD %ld: Thread [%ld] joined (returned %hu)\n", pthread_self(), tid[i], *ret_val);
+    }
+    free(ret_val);
+
 
     // Clean up dynamically allocated variables
     free_bool_arr_2d(universe, rows_per_rank + 2);
 
     // END -Perform a barrier and then leave MPI
-    MPI_Barrier( MPI_COMM_WORLD );
+    MPI_Barrier(MPI_COMM_WORLD);
     MPI_Finalize();
     return 0;
 }
@@ -122,6 +166,12 @@ int main(int argc, char *argv[])
 /***************************************************************************/
 /* Other Functions - You write as part of the assignment********************/
 /***************************************************************************/
+
+void *threadFunction(void *arg) {
+    bool *b = (bool *) arg;
+
+    pthread_exit(b);
+}
 
 void print_some_universe(bool **uni) {
     for (int i = 0; i < 25; i++) {
@@ -174,7 +224,7 @@ bool life_lotto(Gen seed) {
 
 void sync_ghost_rows(bool **old_uni, int rows, int cols, int rank, int num_ranks) {
     bool *tg_row = old_uni[0];
-    bool *bg_row = old_uni[rows-1];
+    bool *bg_row = old_uni[rows - 1];
     MPI_Request tg_req;
     MPI_Request bg_req;
     MPI_Status status1;
@@ -212,19 +262,18 @@ void sync_ghost_rows(bool **old_uni, int rows, int cols, int rank, int num_ranks
 int neighbor_count(int i_in, int j_in, bool **uni, int rows, int cols) {
     int count = 0;
     for (int i = i_in - 1; i < i_in + 2; i++) {
-        for (int j = j_in-1; j < j_in + 2; j++) {
-            if (i > 0 
-                && j > 0 
-                && i < rows 
+        for (int j = j_in - 1; j < j_in + 2; j++) {
+            if (i > 0
+                && j > 0
+                && i < rows
                 && j < cols
-                && !(j==j_in && i==i_in)
+                && !(j == j_in && i == i_in)
                 && uni[i][j] == ALIVE) {
                 count++;
             }
         }
     }
     return count;
-
 }
 
 // 
@@ -236,7 +285,7 @@ bool cell_next_state(int i, int j, bool **uni, int rows, int cols) {
     int neighbors = neighbor_count(i, j, uni, rows, cols);
     int is_alive = uni[i][j];
     if (is_alive == ALIVE) {
-        if (neighbors < 2 || neighbors > 3) 
+        if (neighbors < 2 || neighbors > 3)
             return DEAD;
     } else {
         if (neighbors == 3)
@@ -254,9 +303,9 @@ void init_universe(bool **uni, int rows, int cols) {
 }
 
 bool **alloc_bool_arr_2d(int rows, int cols) {
-    bool **uni = (bool **)calloc(rows, sizeof(bool*));
+    bool **uni = (bool **) calloc(rows, sizeof(bool *));
     for (int i = 0; i < rows; i++) {
-        uni[i] = (bool *)calloc(cols, sizeof(bool));
+        uni[i] = (bool *) calloc(cols, sizeof(bool));
     }
     return uni;
 }
